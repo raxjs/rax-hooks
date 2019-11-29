aa1c58a30f74a6a07c71969a42aecef0ac1a3934
fushen <fushen.jzw@alibaba-inc.com>
2019-11-29 22:17:57 +0800

    feat: add useMountedState

---
+++ packages/useMountedState/src/__tests__/useMountedState.js
@@ -0,0 +1,47 @@
+import { createElement, useState, useEffect } from 'rax';
+import renderer from 'rax-test-renderer';
+import useMountedState from '../index';
+
+const noop = () => {};
+
+describe('useMountedState', () => {
+  beforeEach(function() {
+    jest.useFakeTimers();
+  });
+
+  it('should get component mounted state', () => {
+    let isMounted = noop;
+    function App() {
+      isMounted = useMountedState();
+      return <span>app</span>;
+    }
+
+    renderer.create(<App />);
+
+    jest.runAllTimers();
+    expect(isMounted()).toEqual(true);
+  });
+
+  it('should get component unmounted state', () => {
+    let isMounted = noop;
+    function App() {
+      const [showTest, setShowTest] = useState(true);
+      useEffect(() => {
+        setTimeout(() => {
+          setShowTest(false);
+        });
+      }, 1000);
+      return showTest ? <Test /> : null;
+    }
+
+    function Test() {
+      isMounted = useMountedState();
+      return <span>test</span>;
+    }
+
+    renderer.create(<App />);
+
+    jest.runAllTimers();
+    expect(isMounted()).toEqual(false);
+  });
+});
