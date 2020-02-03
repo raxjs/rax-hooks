77ecf5aaf9373bed1764e842bd5d8899e8ec32b8
fushen <fushen.jzw@alibaba-inc.com>
2020-02-03 23:34:00 +0800

    chore: remove useless rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,5 @@
 module.exports = {
   'extends': [
     'rax'
-  ],
-  'root': true,
-  'rules': {
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        'peerDependencies': true,
-        'devDependencies': [
-          '**/scripts/*.js',
-          '**/__tests__/*.test.js',
-          '**/__tests__/**/*.js',
-          '**/*.config.js',
-          '**/config/*.js',
-          '**/*.conf.js',
-          '**/tests/*.test.js',
-        ],
-      },
-    ],
-  }
+  ]
 };
